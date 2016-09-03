b7d78c4ce46acfff166e280ef1fadaa01ce3d57e
Dell <Dell@DELL-PC>
2016-09-03 10:01:47 +0200

    update changes

--- src/client/app/app.component.ts
+++ src/client/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { ROUTER_DIRECTIVES } from '@angular/router';
 import { HTTP_PROVIDERS } from '@angular/http';
 
-import { Config, NameListService, NavbarComponent, ToolbarComponent } from './shared/index';
+import { Config, NavbarComponent } from './shared/index';
 
 /**
  * This class represents the main application component. Within the @Routes annotation is the configuration of the
@@ -11,9 +11,9 @@ import { Config, NameListService, NavbarComponent, ToolbarComponent } from './sh
 @Component({
   moduleId: module.id,
   selector: 'sd-app',
-  viewProviders: [NameListService, HTTP_PROVIDERS],
+  viewProviders: [HTTP_PROVIDERS],
   templateUrl: 'app.component.html',
-  directives: [ROUTER_DIRECTIVES, NavbarComponent, ToolbarComponent]
+  directives: [ROUTER_DIRECTIVES, NavbarComponent]
 })
 export class AppComponent {
   constructor() {
